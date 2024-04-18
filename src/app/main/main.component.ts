e4b3991f13b05b87c09b1642889dbe6e1b2b1701
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-04-18 07:34:22 -0300

    fix/ajusta TS e explica transformacoes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -30,51 +30,62 @@ export class MainComponent {
    
      let html = this.form.value.content;
    
-     // Replace text-align classes with inline styles
-     html = html.replace(/class="ql-align-(\w+)">([\s\S]*?)<\/p>/g, (match, p1, p2) => {
-         return `style="text-align: ${p1};">${p2}</p>`;
-     });
+     // Substituindo text-align class para inline style
+    
+
+     html = html.replace(/class="ql-align-(\w+)">([\s\S]*?)<\/p>/g, (match: string, p1: string, p2: string) => {
+      return `style="text-align: ${p1};">${p2}</p>`;
+      });
+  
    
-     // Replace font size classes with inline styles
-     html = html.replace(/class="ql-size-(\w+)">([\s\S]*?)<\/p>/g, (match, p1, p2) => {
-         let fontSize = '';
-         switch (p1) {
-             case 'small':
-                 fontSize = '0.75em';
-                 break;
-             case 'normal':
-                 fontSize = '1em';
-                 break;
-             case 'large':
-                 fontSize = '1.5em';
-                 break;
-             case 'huge':
-                 fontSize = '2.5em';
-                 break;
-             default:
-                 break;
-         }
-         return `style="font-size: ${fontSize};">${p2}</p>`;
-     });
+     // Substituindo font-size class para inline style 
+    
+
+    html = html.replace(/class="ql-size-(\w+)">([\s\S]*?)<\/p>/g, (match: string, p1: string, p2: string) => {
+      let fontSize: string = '';
+      switch (p1) {
+          case 'small':
+              fontSize = '0.75em';
+              break;
+          case 'normal':
+              fontSize = '1em';
+              break;
+          case 'large':
+              fontSize = '1.5em';
+              break;
+          case 'huge':
+              fontSize = '2.5em';
+              break;
+          default:
+              break;
+      }
+      return `style="font-size: ${fontSize};">${p2}</p>`;
+    });
+  
    
-     // Add font size to existing style or create new style
-     html = html.replace(/style="(.*?)">([\s\S]*?)<\/p>/g, (match, p1, p2) => {
-         let style = p1;
-         if (!style.includes('font-size')) {
-             style += ` font-size: 1em;`;
-         }
-         return `style="${style}">${p2}</p>`;
-     });
+     // Adiciona font-size a um estilo existente/ou cria 
+
+
+    html = html.replace(/style="(.*?)">([\s\S]*?)<\/p>/g, (match: string, p1: string, p2: string) => {
+      let style: string = p1;
+      if (!style.includes('font-size')) {
+          style += ` font-size: 1em;`;
+      }
+      return `style="${style}">${p2}</p>`;
+    });
+  
+
    
-     // Add text-align to existing style or create new style
-     html = html.replace(/style="(.*?)">([\s\S]*?)<\/p>/g, (match, p1, p2) => {
-         let style = p1;
-         if (!style.includes('text-align')) {
-             style += ` text-align: left;`;
-         }
-         return `style="${style}">${p2}</p>`;
-     });
-   
+     // Adiciona text-align a um style existente ou cria um novo 
+  
+    html = html.replace(/style="(.*?)">([\s\S]*?)<\/p>/g, (match: string, p1: string, p2: string) => {
+      let style: string = p1;
+      if (!style.includes('text-align')) {
+          style += ` text-align: left;`;
+      }
+      return `style="${style}">${p2}</p>`;
+    });
+  
    
    
      const objeto = {
